d40fa9a795bda3df182aa59b1b6c7496d01d6dda
zhongdaoyou <18942371859@163.com>
2017-01-27 15:08:40 +0800

    all the things

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+      resources :resumes
+    end
+  end
+
+  resources :jobs do
+  resources :resumes
+end
+
+resources :welcome do
+
+end
+root 'welcome#index'
+
+
+end
