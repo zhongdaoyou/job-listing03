bbac652e7b3c5d9d519ddb9a01122351038e254e
zhongdaoyou <18942371859@163.com>
2017-04-20 01:11:16 +0800

    implement part 05

--- job-listing05/app/models/job.rb
+++ job-listing05/app/models/job.rb
@@ -3,4 +3,12 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  def render_job_status(job)
+  if job.is_hidden
+    "(Hidden)"
+  else
+    "(Public)"
+  end
+end
 end
